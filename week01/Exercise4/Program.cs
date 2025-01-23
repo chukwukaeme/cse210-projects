0fc76fb7e380104c41132f108842bcc17740e0a4
chukwukaeme <168792086+chukwukaeme@users.noreply.github.com>
2025-01-23 13:44:27 +0100

    list work

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+      List<int> numbers = new List<int>();
+
+      int userNumber = -1;
+      while (userNumber != 0)
+      {
+        Console.Write("Enter a number. Add 0 to quit: ");
+        string userResponse = Console.ReadLine();
+        userNumber = int.Parse(userResponse);
+
+        /*Add all numbers except 0 to the list*/
+        if (userNumber != 0)
+        {
+            numbers.Add(userNumber);
+        }
+      }
+      /*Tp solve for the sum*/
+      int sum = 0;
+      foreach (int number in numbers)
+      {
+        sum += number;
+      }
+      Console.WriteLine($"Then sum is: {sum}");
+      /*To find the average*/
+      float average = ((float)sum) / numbers.Count;
+      Console.WriteLine($"The average is: {average}");
+      /*To know the maximum number*/
+      int max = numbers[0];
+      foreach (int number in numbers)
+      {
+        if (number > max)
+        {
+            max = number;
+        }
+      }
+      Console.WriteLine($"The max is: {max}");
     }
 }
