da7fa15b14dc8ed4cbc485e7bd5a7cda2e7b61fd
chukwukaeme <168792086+chukwukaeme@users.noreply.github.com>
2025-01-23 13:21:42 +0100

    loops work

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Your guess should be between 0 to 11");
+        Random random = new Random();
+        int magicNumber = random .Next(1, 11);
+
+        int guess = 0;
+
+        while(guess != magicNumber)
+        {
+            Console.Write("Guess a number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber > guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed correctly");
+            }
+        }
+
     }
 }
