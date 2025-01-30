8a0b6c1731f44d390e77c218b296344a2826da3f
chukwukaeme <168792086+chukwukaeme@users.noreply.github.com>
2025-01-30 14:01:28 +0100

    still there

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -4,17 +4,27 @@ public class Journal
     public List<Entry> _entries = new List<Entry>();
     
     
-    public void AddEntry(Entry newEntry)
+    public void AddEntry(string prompt, string response)
     {
-
+        Entry newEntry = new Entry(prompt, response); //create a new entry
+        _entries.Add(newEntry); //To add it to the list
     }
     public void DisplayAll()
     {
-
+        if (_entries.Count == 0)
+        {
+            Console.WriteLine("No entries to display.");
+            return;
+        }
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
     }
     public void SaveToFile(string file)
     {
-
+        List<string> entryDataList
     }
     public void LoadFromFile(string file)
     {
