8bc8bf53a3f7863ec96c27d426ba6e9770660f6d
chukwukaeme <168792086+chukwukaeme@users.noreply.github.com>
2025-01-30 13:40:32 +0100

    work

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -5,12 +5,24 @@ public class Entry
     public string _promptText;
     public string _entryText;
     
+    public Entry(string _promptTextromptText, string _entryText)
+    {
+        _date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        _promptText = promptText;
+        _entryText = entryText;
+    }
     public Entry()
     {
 
     }
     public void Display()
     {
-        
+        Console.WriteLine($"[{_date}]");
+        Console.WriteLine($"Prompt: {_promptText}")
+        Console.WriteLine($"Entry: {_entryText}")
+    }
+    public override string ToString()
+    {
+        return $"{_date} | {_promptext} | {_entryText} ";
     }
 }
